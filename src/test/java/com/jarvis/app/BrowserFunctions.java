fcecc65883fc0af28afe65eeb7e06e295858c2c0
rolbemiranda <rolbemiranda@gmail.com>
2014-10-19 10:51:36 +0800

    modified

--- src/test/java/com/jarvis/app/BrowserFunctions.java
+++ src/test/java/com/jarvis/app/BrowserFunctions.java
@@ -51,6 +51,7 @@ public class BrowserFunctions {
         Selenium selenium = new DefaultSelenium("localhost", 4444, "*firefox", URL);
         selenium.start();
         selenium.windowMaximize();
+        assert selenium.captureScreenshot("shot.jpg")
         assert selenium.isTextPresent("kass");
     }
 
